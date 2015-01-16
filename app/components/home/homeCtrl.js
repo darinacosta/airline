6e80530e47e996a8c4e08b4219966cfc37f6c852
unknown <djacosta@ITI-1109L.cityofno.com>
2015-01-16 17:24:27 -0600

    changes

--- app/components/home/homeCtrl.js
+++ app/components/home/homeCtrl.js
@@ -7,6 +7,8 @@ define(['app'], function (app) {
        'url': 'http://airlinehighway.tumblr.com/'},
       {'alias': 'youtube',
        'url': 'https://www.youtube.com/playlist?list=PLTHF5mLiW_UjklgeWMaL_SM-WVqe_CoDa'},
+       {'alias': 'germancoast',
+       'url': 'http://germancoast.verylongroad.com/'},
       {'alias': 'oldland',
        'url': 'http://oldland.verylongroad.com'}
     ];
@@ -14,7 +16,6 @@ define(['app'], function (app) {
     $scope.publications = [
       {'alias': 'photography',
        'url': 'http://cargocollective.com/airline'},
-
     ]
 
   });
