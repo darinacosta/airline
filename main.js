92e8ae7e1f0561e958ada7531cd0eb9f28e74a84
unknown <djacosta@ITI-1109L.cityofno.com>
2015-01-20 09:30:22 -0600

    move to root

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 require.config({
     baseUrl: "",    
     paths: {
-        'angular': '../node_modules/angular/angular',
+        'angular': 'node_modules/angular/angular',
         'angularAMD': '//cdn.jsdelivr.net/angular.amd/0.2.0/angularAMD.min',
         'angularRoute': '//ajax.googleapis.com/ajax/libs/angularjs/1.2.16/angular-route.min'
     },
